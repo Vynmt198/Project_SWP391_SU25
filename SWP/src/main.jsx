0c3bb6ab9af7285067544ba4db530e7afa817de6
LysTruong <truongly439@gmail.com>
2025-05-21 16:03:09 +0700

    sign up page

--- SWP/src/main.jsx
+++ SWP/src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App';
-import Login from './loginpages/login';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Login from "./loginpages/login";
+import Signup from "./signuppages/signup";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/login" element={<Login/>} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
       <Route path="/*" element={<App />} />
     </Routes>
   </BrowserRouter>
